595dd8acf4039ea3c3d623e80f473756023ef0b0
MrGeorgeG <georgegan94@gmail.com>
2020-12-10 21:32:03 -0500

    Initial commit

---
+++ Assets/Scripts/Battle/BattleSystem.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { Start, PlayerAction, PlayerMove, EnemyMove, Busy}
+
+public class BattleSystem : MonoBehaviour
+{
+    [SerializeField] BattleUnit playerUnit;
+    [SerializeField] BattleUnit enemyUnit;
+    [SerializeField] BattleHud playerHud;
+    [SerializeField] BattleHud enemyHud;
+    [SerializeField] BattleDialogBox dialogBox;
+
+    public event Action<bool> OnBattleOver;
+
+    BattleState state;
+    int currentAction;
+    int currentMove;
+
+    public void StartBattle()
+    {
+        StartCoroutine(SetupBattle());
+    }
+
+    public IEnumerator SetupBattle()
+    {
+        playerUnit.Setup();
+        enemyUnit.Setup();
+        playerHud.SetData(playerUnit.Character);
+        enemyHud.SetData(enemyUnit.Character);
+
+        dialogBox.SetMoveNames(playerUnit.Character.Moves);
+
+        yield return dialogBox.TypeDialog($"A wild {enemyUnit.Character.Base.Name} appeared.");
+
+        PlayerAction();
+    }
+
+    void PlayerAction()
+    {
+        state = BattleState.PlayerAction;
+        StartCoroutine(dialogBox.TypeDialog("Chosse an action"));
+        dialogBox.EnableActionSelector(true);
+    }
+
+    void PlayerMove()
+    {
+        state = BattleState.PlayerMove;
+        dialogBox.EnableActionSelector(false);
+        dialogBox.EnableDialogText(false);
+        dialogBox.EnableMoveSelector(true);
+    }
+
+    IEnumerator PerformPlayerMove()
+    {
+        state = BattleState.Busy;
+
+        var move = playerUnit.Character.Moves[currentMove];
+        yield return dialogBox.TypeDialog($"{playerUnit.Character.Base.Name} used {move.Base.Name}");
+
+        var damageDetails = enemyUnit.Character.TakeDamage(move, playerUnit.Character);
+        yield return enemyHud.UpdateHP();
+        yield return ShowDamageDetails(damageDetails);
+
+        if (damageDetails.Fainted)
+        {
+            yield return dialogBox.TypeDialog($"{enemyUnit.Character.Base.Name} Fainted");
+
+            yield return new WaitForSeconds(2f);
+            OnBattleOver(true);
+        }
+        else
+        {
+            StartCoroutine(EnemyMove());
+        }
+    }
+
+    IEnumerator EnemyMove()
+    {
+        state = BattleState.EnemyMove;
+
+        var move = enemyUnit.Character.GetRandomMove();
+        yield return dialogBox.TypeDialog($"{enemyUnit.Character.Base.Name} used {move.Base.Name}");
+
+        var damageDetails = playerUnit.Character.TakeDamage(move, playerUnit.Character);
+        yield return playerHud.UpdateHP();
+        yield return ShowDamageDetails(damageDetails);
+
+        if (damageDetails.Fainted)
+        {
+            yield return dialogBox.TypeDialog($"{playerUnit.Character.Base.Name} Fainted");
+
+            yield return new WaitForSeconds(2f);
+            OnBattleOver(false);
+        }
+        else
+        {
+            PlayerAction();
+        }
+    }
+
+    IEnumerator ShowDamageDetails(DamageDetails damageDetails)
+    {
+        if (damageDetails.Critical > 1f)
+        {
+            yield return dialogBox.TypeDialog("A critical hit!");
+        }
+        if (damageDetails.TypeEffectiveness > 1)
+        {
+            yield return dialogBox.TypeDialog("It is super effective!");
+        }
+        else if (damageDetails.TypeEffectiveness < 1f)
+        {
+            yield return dialogBox.TypeDialog("It is not very effective!");
+        }
+    }
+
+    public void HandleUpdate()
+    {
+        if (state == BattleState.PlayerAction)
+        {
+            HandleActionSelection();
+        }
+        else if (state == BattleState.PlayerMove)
+        {
+            HandleMoveSelection();
+        }
+    }
+
+    void HandleActionSelection()
+    {
+        if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentAction < 1)
+            {
+                ++currentAction;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentAction > 0)
+            {
+                --currentAction;
+            }
+        }
+
+        dialogBox.UpdateActionSelection(currentAction);
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            if (currentAction == 0)
+            {
+                //Fight
+                PlayerMove();
+            }
+            else if (currentAction == 1)
+            {
+                //Run
+            }
+        }
+    }
+
+    void HandleMoveSelection()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if (currentMove < playerUnit.Character.Moves.Count - 1)
+            {
+                ++currentMove;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            if (currentMove > 0)
+            {
+                --currentMove;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            if (currentMove < playerUnit.Character.Moves.Count - 2)
+            {
+                currentMove += 2;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (currentMove > 1)
+            {
+                currentMove -= 2;
+            }
+        }
+
+        dialogBox.UpdateMoveSelection(currentMove, playerUnit.Character.Moves[currentMove]);
+
+        if(Input.GetKeyDown(KeyCode.Z))
+        {
+            dialogBox.EnableMoveSelector(false);
+            dialogBox.EnableDialogText(true);
+            StartCoroutine(PerformPlayerMove());
+        }
+    }
+}
---
+++ Assets/Scripts/Characters/CharacterBase.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Character", menuName = "Character/Create new pokemon")]
+public class CharacterBase : ScriptableObject
+{
+    [SerializeField]
+    string name;
+
+    [TextArea]
+    [SerializeField]
+    string desciption;
+
+    [SerializeField]
+    Sprite frontSprite;
+    [SerializeField]
+    Sprite backSprite;
+
+    [SerializeField]
+    CharacterType type1;
+    [SerializeField]
+    CharacterType type2;
+
+    //Base Stats
+    [SerializeField]
+    int maxHp;
+    [SerializeField]
+    int attack;
+    [SerializeField]
+    int defense;
+    [SerializeField]
+    int spAttack;
+    [SerializeField]
+    int spDefense;
+    [SerializeField]
+    int speed;
+
+    [SerializeField]
+    List<LearnableMove> learnableMoves;
+
+    public string Name { get { return name; } }
+    
+    public string Description { get { return desciption; } }
+
+    public Sprite FrontSprite { get { return frontSprite; } }
+
+    public Sprite BackSprite { get { return backSprite; } }
+
+    public CharacterType Type1 { get { return type1; } }
+
+    public CharacterType Type2 { get { return type1; } }
+
+    public int MaxHp { get { return maxHp; } }
+
+    public int Attack { get { return attack; } }
+
+    public int Defense { get { return defense; } }
+
+    public int SpAttack { get { return spAttack; } }
+
+    public int SpDefense { get { return spDefense; } }
+
+    public int Speed { get { return speed; } }
+
+    public List<LearnableMove> LearnableMoves { get { return learnableMoves; } }
+}
+
+[System.Serializable]
+public class LearnableMove
+{
+    [SerializeField]
+    MoveBase moveBase;
+    [SerializeField]
+    int level;
+
+    public MoveBase Base { get { return moveBase; } }
+
+    public int Level { get { return level; } }
+
+}
+
+public enum CharacterType
+{
+    None,
+    Normal,
+    Fire,
+    Water,
+    Electric,
+    Grass,
+    Ice,
+    Fighting,
+    Poison,
+    Ground,
+    Flying,
+    Psychic,
+    Bug,
+    Rock,
+    Ghost,
+    Dragon
+}
+
+public class TypeChart
+{
+    static float[][] chart = 
+    { 
+       //Has to be same order as PokemonType class
+        //                       Nor   Fir   Wat   Ele   Gra   Ice   Fig   Poi   Gro   Fly   Psy   Bug   Roc   Gho   Dra   Dar  Ste    Fai
+        /*Normal*/  new float[] {1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   0.5f, 0,    1f,   1f,   0.5f, 1f},
+        /*Fire*/    new float[] {1f,   0.5f, 0.5f, 1f,   2f,   2f,   1f,   1f,   1f,   1f,   1f,   2f,   0.5f, 1f,   0.5f, 1f,   2f,   1f},
+        /*Water*/   new float[] {1f,   2f,   0.5f, 1f,   0.5f, 1f,   1f,   1f,   2f,   1f,   1f,   1f,   2f,   1f,   0.5f, 1f,   1f,   1f},
+        /*Electric*/new float[] {1f,   1f,   2f,   0.5f, 0.5f, 1f,   1f,   1f,   0f,   2f,   1f,   1f,   1f,   1f,   0.5f, 1f,   1f,   1f},
+        /*Grass*/   new float[] {1f,   0.5f, 2f,   1f,   0.5f, 1f,   1f,   0.5f, 2f,   0.5f, 1f,   0.5f, 2f,   1f,   0.5f, 1f,   0.5f, 1f},
+        /*Ice*/     new float[] {1f,   0.5f, 0.5f, 1f,   2f,   0.5f, 1f,   1f,   2f,   2f,   1f,   1f,   1f,   1f,   2f,   1f,   0.5f, 1f},
+        /*Fighting*/new float[] {2f,   1f,   1f,   1f,   1f,   2f,   1f,   0.5f, 1f,   0.5f, 0.5f, 0.5f, 2f,   0f,   1f,   2f,   2f,   0.5f},
+        /*Poison*/  new float[] {1f,   1f,   1f,   1f,   2f,   1f,   1f,   0.5f, 0.5f, 1f,   1f,   1f,   0.5f, 0.5f, 1f,   1f,   0f,   2f},
+        /*Ground*/  new float[] {1f,   2f,   1f,   2f,   0.5f, 1f,   1f,   2f,   1f,   0f,   1f,   0.5f, 2f,   1f,   1f,   1f,   2f,   1f},
+        /*Flying*/  new float[] {1f,   1f,   1f,   0.5f, 2f,   1f,   2f,   1f,   1f,   1f,   1f,   2f,   0.5f, 1f,   1f,   1f,   0.5f, 1f},
+        /*Psychic*/ new float[] {1f,   1f,   1f,   1f,   1f,   1f,   2f,   2f,   1f,   1f,   0.5f, 1f,   1f,   1f,   1f,   0f,   0.5f, 1f},
+        /*Bug*/     new float[] {1f,   0.5f, 1f,   1f,   2f,   1f,   0.5f, 0.5f, 1f,   0.5f, 2f,   1f,   1f,   0.5f, 1f,   2f,   0.5f, 0.5f},
+        /*Rock*/    new float[] {1f,   2f,   1f,   1f,   1f,   2f,   0.5f, 1f,   0.5f, 2f,   1f,   2f,   1f,   1f,   1f,   1f,   0.5f, 1f},
+        /*Ghost*/   new float[] {0f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   0.5f, 1f,   1f,   2f,   1f,   0.5f, 1f,   1f},
+        /*Dragon*/  new float[] {1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   1f,   2f,   1f,   0.5f, 0f},
+        /*Dark*/    new float[] {1f,   1f,   1f,   1f,   1f,   1f,   0.5f, 1f,   1f,   1f,   2f,   1f,   1f,   2f,   1f,   0.5f, 1f,   0.5f},
+        /*Steel*/   new float[] {1f,   0.5f, 0.5f, 0.5f, 1f,   2f,   1f,   1f,   1f,   1f,   1f,   2f,   0.5f, 1f,   1f,   1f,   0.5f, 2f},
+        /*Fairy*/   new float[] {1f,   0.5f, 1f,   1f,   1f,   1f,   2f,   0.5f, 1f,   1f,   1f,   1f,   1f,   1f,   2f,   2f,   0.5f, 1f},
+    };
+
+    public static float GetEffectiveness(CharacterType attackType, CharacterType defenseType)
+    {
+        if (attackType == CharacterType.None || defenseType == CharacterType.None)
+            return 1;
+        
+        int row = (int)attackType - 1;
+        int col = (int)defenseType - 1;
+
+        return chart[row][col];
+    }
+}
